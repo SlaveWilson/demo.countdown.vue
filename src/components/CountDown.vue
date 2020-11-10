ae17da09e6864853952ac8f3cc59121a280a52e5
Tommy Ng <tommynhm@gmail.com>
2020-11-10 16:27:28 +0800

    Update CountDown.vue

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -2,16 +2,16 @@
 .container
   .flex.justify-between
     .countdown-box
-      BaseCard 1
+      BaseCard {{ days }}
       p Days
     .countdown-box
-      BaseCard 1
+      BaseCard {{ hours }}
       p Hours
     .countdown-box
-      BaseCard 1
-      p Minuets
+      BaseCard {{ minutes }}
+      p Minutes
     .countdown-box
-      BaseCard 1
+      BaseCard {{ seconds }}
       p Seconds
 </template>
 
@@ -23,11 +23,32 @@ export default {
   },
   data() {
     return {
-      currentTime: null,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
     };
   },
   created() {
-    this.currentTime = new Date();
+    setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  },
+  methods: {
+    updateCountdown() {
+      const currentTime = new Date();
+      const targetTime = new Date(2021, 0, 0, 0, 0, 0, 0);
+      const diff = targetTime - currentTime;
+      let remainder = Math.round(diff / 1000);
+
+      this.days = Math.floor(remainder / 60 / 60 / 24);
+      remainder %= 60 * 60 * 24;
+      this.hours = Math.floor(remainder / 60 / 60);
+      remainder %= 60 * 60;
+      this.minutes = Math.floor(remainder / 60);
+      remainder %= 60;
+      this.seconds = Math.floor(remainder);
+    },
   },
 };
 </script>
